8c94d54ee02dc689871d1e01e3ee22f7e6ea357c
Jason Lin <jlin51@illinois.edu>
2016-01-19 13:06:53 -0600

    Initial Commit

---
+++ typings/tsd.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="bcrypt/bcrypt.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="jsonwebtoken/jsonwebtoken.d.ts" />
+/// <reference path="mongoose/mongoose.d.ts" />
+/// <reference path="morgan/morgan.d.ts" />
+/// <reference path="node/node.d.ts" />
