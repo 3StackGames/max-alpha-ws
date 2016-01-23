a503b9f6f9ab026dcad4907466e85799f054cf22
Jason Lin <jlin51@illinois.edu>
2016-01-23 15:39:24 -0600

    GET cards by userId working

--- controllers/cards.js
+++ controllers/cards.js
@@ -13,18 +13,42 @@ var INVALID_PROPERTIES = 'Invalid Properties'
 var DECK_NOT_FOUND_ERROR = new Error(INVALID_PROPERTIES, 'Deck not found')
 var DECK_NOT_FOUND_RESPONSE = new Response(null, DECK_NOT_FOUND_ERROR)
 
-var GET_DECK_CARDS = [
-    {
-        
-    }
-]
+var USER_NOT_FOUND_ERROR = new Error(INVALID_PROPERTIES, 'User not found')
+var USER_NOT_FOUND_RESPONSE = new Response(null, USER_NOT_FOUND_ERROR)
+
+var ID_INVALID_ERROR = new Error(INVALID_PROPERTIES, 'Either deckId or userId wasn\'t a valid object id')
+var ID_INVALID_RESPONSE = new Response(null, ID_INVALID_ERROR)
+
+var ILLEGAL_COMBINATION_ERROR = new Error(INVALID_PROPERTIES, 'Both deckId and userId were supplied. Only one is allowed at a time')
+var ILLEGAL_COMBINATION_RESPONSE = new Response(null, ILLEGAL_COMBINATION_ERROR)
+
 mod.get = function (req, res) {
     var deckId = req.query.deckId
     var userId = req.query.userId
     
+    //make sure both aren't provided
+    if(deckId && userId) {
+        res.status(400).json(ILLEGAL_COMBINATION_RESPONSE)
+        return
+    }
+    
+    try {
+        if(deckId) {
+            deckId = new mongoose.Types.ObjectId(deckId)
+        } else if(userId) {
+            userId = new mongoose.Types.ObjectId(userId)
+        }
+    } catch (err) {
+        res.status(400).json(ID_INVALID_RESPONSE)
+        return
+    }
+    
     if(!deckId && !userId) {
         Card.find(function(err, cards) {
-            res.json(new Response(cards))
+            var processedCards = cards.map(function(card) {
+              return card.process()  
+            })
+            res.json(new Response(processedCards))
         })
     } else if(deckId) {
         Deck.aggregate([
@@ -82,6 +106,49 @@ mod.get = function (req, res) {
             res.json(new Response(processedCards))
         })
     } else if(userId) {
-        
+        User.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $unwind: "$cards"
+            },
+            {
+                $group: {
+                    _id: "$cards"
+                }
+            },
+            {
+            $lookup: {
+                from: "cards",
+                localField: "_id",
+                foreignField: "_id",
+                as: "cards"
+            }
+            },
+            {
+                $unwind: "$cards"
+            },
+            { 
+                $group: { 
+                    _id: "_id", 
+                    cards: { $push : "$cards" }
+                }
+            }
+        ], function (err, users) {
+            if (err) throw err
+            if(users.length < 1) {
+                res.status(404).json(USER_NOT_FOUND_RESPONSE)
+                return
+            }
+            var cards = users[0].cards
+            var processedCards = cards.map(function(card) {
+                return Card.process(card)
+            })
+            
+            res.json(new Response(processedCards))
+        })
     }
 }
