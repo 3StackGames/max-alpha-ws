382c0664f40d652d756f77289677e6e7cf34e88c
Jason Lin <jlin51@illinois.edu>
2016-01-26 16:29:59 -0600

    changed url

--- routes/router.js
+++ routes/router.js
@@ -3,18 +3,13 @@ var router = express.Router()
 
 var v1 = require('./v1/router')
 
-/* API Documentation */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
-});
-
 /* API Welcome */
-router.get('/api', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.json({
-      message: 'Make sure to select an api version. For example: /api/v1'
+      message: 'Make sure to select an api version. For example: /v1'
   });
 });
 
-router.use('/api/v1', v1)
+router.use('/v1', v1)
 
 module.exports = router;
