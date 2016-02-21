a29ef21fb00d89df306c4ed20a81f84a1396dc5d
Jason Lin <jlin51@illinois.edu>
2016-02-20 21:55:56 -0600

    cors bugfix

--- app.js
+++ app.js
@@ -32,11 +32,15 @@ app.set('view engine', 'jade')
  * Custom
  */
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.use(allowCrossDomain)
 
 /**
  * Default
